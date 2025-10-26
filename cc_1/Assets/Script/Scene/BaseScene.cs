5dc23864bcce2a453850b456b14bdc5e3a0118ec
tohru97 <tohru970211@gmail.com>
2025-10-26 20:32:49 +0900

    씬 수정중

--- cc_1/Assets/Script/Scene/BaseScene.cs
+++ cc_1/Assets/Script/Scene/BaseScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class BaseScene : MonoBehaviour
 {
-    protected virtual async UniTaskVoid Awake()
+    protected async UniTaskVoid Awake()
     {
         await LoadAsync();
         AppManager.Instance.OnSceneLoad(this);
