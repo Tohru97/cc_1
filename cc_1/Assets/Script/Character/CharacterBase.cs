bf48855998eaa85b07e44f1ce0632f8f199e3a7e
tohru97 <tohru970211@gmail.com>
2025-10-27 23:44:38 +0900

    게임 코어루프 순서 기능 작업

--- cc_1/Assets/Script/Character/CharacterBase.cs
+++ cc_1/Assets/Script/Character/CharacterBase.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class CharacterBase : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string _characterName;
+    public int _hp;
+    public int _power;
+    
 }
--- cc_1/Assets/Script/Match/MatchControllerBase.cs
+++ cc_1/Assets/Script/Match/MatchControllerBase.cs
@@ -1,10 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MatchControllerBase : MonoBehaviour
 {
+    public List<CardBase> _playerDeck = new List<CardBase>();
+    public List<CardBase> _opponentDeck = new List<CardBase>();
+
     public void StartMatch()
     {
 
+    }
+
+    public void SelectCharacter()
+    {
+        
+    }
+
+    public void DrawCard()
+    {
+
+    }
+
+    public void SelectCard()
+    {
+
+    }
+
+    public void SetCardOrder()
+    {
+
+    }
+    
+    public void CardOpen()
+    {
+        
     }
     
     public void EndMatch()
